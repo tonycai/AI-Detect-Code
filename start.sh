91dda777ac61e2e54b887ca8c9b5187a66f78783
summer.jia <luojia@baixing.com>
2019-02-18 11:33:32 +0800

    调整启动脚本

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-docker-compose -f docker-compose.prod.yml up --force-recreate --no-deps -d $1
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --force-recreate --no-deps -d server
